e2a0e189b0f6abdb4459af93add435cad43b5985
admishka <mu@tabletr.ru>
2022-10-15 14:27:20 +0300

    Задача 2

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int A = 0;
+int Z = 0;
+int K = 10;
+int N = 1;
+int D = 0;
+Console.WriteLine("Введите число А:");
+int.TryParse(Console.ReadLine()!, out A);
+for (int i = 1; i < 10; i++)
+{
+    D = A%K/N;
+    Z = Z+D;
+    K = K*10;
+    N = N*10;
+}
+Console.WriteLine($"Сумма равна {Z}");
